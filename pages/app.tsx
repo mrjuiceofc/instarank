f3c5f644d2cced47e914b955518586ea562a5a52
Theryston <fuktodo2@gmail.com>
2022-12-20 15:43:28 -0300

    fix: add update plan button

--- pages/app.tsx
+++ pages/app.tsx
@@ -1,11 +1,23 @@
 import { useRouter } from 'next/router';
 import { useEffect } from 'react';
 import styled from 'styled-components';
+import { Button } from '../lib/components/Botton';
 import useAuth from '../lib/hooks/useAuth';
+import useGlobal from '../lib/hooks/useGlobal';
+import prisma from '../lib/prisma';
 import pxToRem from '../lib/utils/pxToRem';
 
-export default function App() {
-  const { user, isLoading } = useAuth();
+type Props = {
+  premiumPlan: {
+    id: string;
+    name: string;
+    price: number;
+    monthlyLimit: number;
+  };
+};
+
+export default function App({ premiumPlan }: Props) {
+  const { user, isLoading, changePlan } = useAuth();
   const route = useRouter();
 
   useEffect(() => {
@@ -16,11 +28,36 @@ export default function App() {
 
   return (
     <Wrapper>
+      {user && user.plan.name === 'free' && (
+        <FloatButton onClick={() => changePlan(premiumPlan.name)}>
+          ganhe {premiumPlan.monthlyLimit.toLocaleString('pt-BR')} ordens
+        </FloatButton>
+      )}
       <p>
         Aqui ficará toda a ferramenta do instarank, e ela está vindo em-breve
       </p>
     </Wrapper>
   );
+}
+
+export async function getStaticProps() {
+  const plan = await prisma.plan.findFirst({
+    where: {
+      deletedAt: null,
+      name: 'premium',
+    },
+  });
+
+  return {
+    props: {
+      premiumPlan: {
+        id: plan.id,
+        name: plan.name,
+        price: plan.price,
+        monthlyLimit: plan.monthlyLimit,
+      },
+    },
+  };
 }
 
 const Wrapper = styled.div`
@@ -32,4 +69,12 @@ const Wrapper = styled.div`
   min-height: 100vh;
   text-align: center;
   padding: ${pxToRem(38)};
+`;
+
+const FloatButton = styled(Button)`
+  position: fixed;
+  max-width: ${pxToRem(211)};
+  left: ${pxToRem(6)};
+  bottom: ${pxToRem(6)};
+  filter: drop-shadow(4px 4px 4px rgba(0, 0, 0, 0.25));
 `;
