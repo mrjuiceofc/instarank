7b1725c89bfdc434b95b9e463e51520bb6336d8c
Theryston <fuktodo2@gmail.com>
2023-01-13 20:46:34 -0300

    fix: event return if there are not window gtag

--- lib/gtag.ts
+++ lib/gtag.ts
@@ -1,12 +1,18 @@
 export const GA_TRACKING_ID = process.env.NEXT_PUBLIC_GA_ID;
 
 export const pageview = (url) => {
+  if (typeof window === 'undefined') return;
+  if (!(window as any).gtag) return;
+
   (window as any).gtag('config', GA_TRACKING_ID, {
     page_path: url,
   });
 };
 
 export const event = ({ action, category, label, value }) => {
+  if (typeof window === 'undefined') return;
+  if (!(window as any).gtag) return;
+
   (window as any).gtag('event', action, {
     event_category: category,
     event_label: label,
